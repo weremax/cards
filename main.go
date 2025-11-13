55aa20a46acf8a007c13326373e1fe29c454949e
weremax <hardcode45@yahoo.com>
2025-11-13 17:49:31 -0500

    added reading and writing to file

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 func main() {
-	cards := newDeck()
+	// cards := newDeck()
+	// fmt.Println(cards.toString())
+	// cards.saveToFile("my_cards")
+	cards := newDeckFromFile("my_cards")
+	cards.print()
 
-	hand, remainingCards := deal(cards, 5)
+	// hand, remainingCards := deal(cards, 5)
 
-	hand.print()
-	remainingCards.print()
+	// hand.print()
+	// remainingCards.print()
 
 	// cards.print()
 }
