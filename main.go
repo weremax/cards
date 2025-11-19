ecde4886718d9b2c96f03ecaa7f8c16c66e8aa3a
weremax <hardcode45@yahoo.com>
2025-11-18 21:37:30 -0500

    finalized the code and added testing

--- main.go
+++ main.go
@@ -1,16 +1,7 @@
 package main
 
 func main() {
-	// cards := newDeck()
-	// fmt.Println(cards.toString())
-	// cards.saveToFile("my_cards")
-	cards := newDeckFromFile("my_cards")
+	cards := newDeck()
+	cards.shuffle()
 	cards.print()
-
-	// hand, remainingCards := deal(cards, 5)
-
-	// hand.print()
-	// remainingCards.print()
-
-	// cards.print()
 }
